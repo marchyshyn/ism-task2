396fa1b00cdc6b61c0bae7f5c35b3f34bb6ec2f3
marchyshyn <amintux@gmail.com>
2015-08-28 17:03:36 +0300

    ism-touch

--- script.js
+++ script.js
@@ -1,36 +1,84 @@
 function up () {
-	$(document).on({
-		mouseenter: function() {
-			$(this).stop().animate({top: '-50px'}, 600, 'easeOutBounce', function(){
-				$('.mess', $(this).parent()).animate({opacity: 1}, 100);
-			});
+	
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		$(".slider-img").on("touchstart",function (event){
+			event.stopPropagation();
+			$('.slider-img').removeClass('pack-up');
+			$('.slider-img').stop().animate({top: '0px'}, 600, 'easeOutBounce');
 			$('.shadow', $(this).parent()).removeClass('shadow-scale-back');
-			$('.shadow', $(this).parent()).addClass('shadow-scale');				
-		},
-		mouseleave: function() {
-			$(this).stop().animate({top: '0px'}, 600, 'easeOutBounce');
-			$('.mess', $(this).parent()).animate({opacity: 0}, 300);
-			$('.shadow', $(this).parent()).addClass('shadow-scale-back');
-			$('.shadow', $(this).parent()).removeClass('shadow-scale');
-		}
-	}, '.slider-img');
+			$('.shadow', $(this).parent()).addClass('shadow-scale');
+			$('.mess').animate({opacity: 0}, 300);
+			$(this).stop().animate({top: '-50px'}, 600, 'easeOutBounce', function(){
+					$('.mess', $(this).parent()).animate({opacity: 1}, 100);
+				});
+			$(this).addClass('pack-up');
+
+			$(this).on('touchstart', function () {
+				//event.stopPropagation();
+
+				if ($(this).css('top') === '-50px') {
+				$('.popup-window').css('display', 'block');	
+				$('.choose-pack').children().remove();
+				$(this).clone().appendTo('.choose-pack');
+			}
+
+			// if ($(this).hasClass('pack-up') == true) {
+			// 	$('.popup-window').css('display', 'block');	
+			// 	$('.choose-pack').children().remove();
+			// 	$(this).clone().appendTo('.choose-pack');
+			// }
+		});
+			
+		});
+		$('.popup-bg, header, .close-popup').on('touchstart', function() {
+			$('.popup-window').css('display', 'none');	
+			$('.slider-img').stop().animate({top: '0px'}, 600, 'easeOutBounce');
+		});
+
+	}
+
+	else {
+		alert('pc');
+		console.log('pc');
+		$(document).on({
+			mouseenter: function() {
+				$(this).stop().animate({top: '-50px'}, 600, 'easeOutBounce', function(){
+					$('.mess', $(this).parent()).animate({opacity: 1}, 100);
+				});
+				$('.shadow', $(this).parent()).removeClass('shadow-scale-back');
+				$('.shadow', $(this).parent()).addClass('shadow-scale');				
+			},
+			mouseleave: function() {
+				$(this).stop().animate({top: '0px'}, 600, 'easeOutBounce');
+				$('.mess', $(this).parent()).animate({opacity: 0}, 300);
+				$('.shadow', $(this).parent()).addClass('shadow-scale-back');
+				$('.shadow', $(this).parent()).removeClass('shadow-scale');
+			}
+		}, '.slider-img');
+		console.log('PC');
+
+		function packClick () {
+			$(document).on({
+				click: function() {
+					$('.popup-window').css('display', 'block');	
+					$('.choose-pack').children().remove();
+					$(this).clone().appendTo('.choose-pack');		
+				}
+			}, '.slider-img');
+		};
+
+		function closePopup () {
+			$('.popup-bg, header, .close-popup').click(function() {
+				$('.popup-window').css('display', 'none');	
+			});
+		};
+		packClick();
+		closePopup();
+	}
+
 };
 
-function packClick () {
-	$(document).on({
-		click: function() {
-			$('.popup-window').css('display', 'block');	
-			$('.choose-pack').children().remove();
-			$(this).clone().appendTo('.choose-pack');		
-		}
-	}, '.slider-img');
-};
 
-function closePopup () {
-	$('.popup-bg, header, .close-popup').click(function() {
-		$('.popup-window').css('display', 'none');	
-	});
-};
 
 function showInfo () {
 	$('.pakje-info').animate({opacity: 0}, 300, function(){
@@ -52,59 +100,59 @@ function hideInfo () {
 			$('.logo').css('display', 'block');
 		}, 300);
 		$('.pakje-info').css('display', 'block');
-	$('.pakje-info').animate({opacity: 1}, 400);
+		$('.pakje-info').animate({opacity: 1}, 400);
 	});
-		
+
 };
 
 function valid () {
-	     $('#firstName, #lastName, #email').unbind().blur( function(){
-         var id = $(this).attr('id');
-         var val = $(this).val();
+	$('#firstName, #lastName, #email').unbind().blur( function(){
+		var id = $(this).attr('id');
+		var val = $(this).val();
 
-       switch(id)
-       {
-             case 'firstName':
-                var rv_name = /^[a-zA-Zа-яА-Я]+$/;
-                if(val.length > 2 && val != '' && rv_name.test(val))
-                {
-                   $(this).addClass('not_error');
-                }
-                else
-                {
-                   $(this).removeClass('not_error').addClass('error');
-                }
-            break;
+		switch(id)
+		{
+			case 'firstName':
+			var rv_name = /^[a-zA-Zа-яА-Я]+$/;
+			if(val.length > 2 && val != '' && rv_name.test(val))
+			{
+				$(this).addClass('not_error');
+			}
+			else
+			{
+				$(this).removeClass('not_error').addClass('error');
+			}
+			break;
 
-            case 'lastName':
-                var rv_name = /^[a-zA-Zа-яА-Я]+$/;
-                if(val.length > 2 && val != '' && rv_name.test(val))
-                {
-                   $(this).addClass('not_error');
-                }
-                else
-                {
-                   $(this).removeClass('not_error').addClass('error');
-                }
-            break;
+			case 'lastName':
+			var rv_name = /^[a-zA-Zа-яА-Я]+$/;
+			if(val.length > 2 && val != '' && rv_name.test(val))
+			{
+				$(this).addClass('not_error');
+			}
+			else
+			{
+				$(this).removeClass('not_error').addClass('error');
+			}
+			break;
 
-           case 'email':
-               var rv_email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
-               if(val != '' && rv_email.test(val))
-               {
-                  $(this).addClass('not_error');
-               }
-               else
-               {
-                  $(this).removeClass('not_error').addClass('error')
-               }
-           break;
-       }
-     }); 
+			case 'email':
+			var rv_email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+			if(val != '' && rv_email.test(val))
+			{
+				$(this).addClass('not_error');
+			}
+			else
+			{
+				$(this).removeClass('not_error').addClass('error')
+			}
+			break;
+		}
+	}); 
 };
 
 function ieRadius () {
-  var myNav = navigator.userAgent.toLowerCase();
+	var myNav = navigator.userAgent.toLowerCase();
 	var ie = (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
 	if (ie == 9) {
 		$('.button').css({'background': 'url(assets/button.png)'});
